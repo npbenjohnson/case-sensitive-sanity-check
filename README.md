f0d0e114bf50ebd292cc9ddc3dd88622002bae4b
bjohnson <npbenjohnson@gmail.com>
2016-04-23 21:16:45 -0700

    wow. such detection, much debug!

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # case-sensitive-sanity-check
-Middleware to use during development that checks for case-mismatch on web requests
+
+Middleware to use during development that checks for case-mismatch on web
+requests.
+
+## Options
+
+- ### `baseUrlPath` - string
+
+  optional base path of urls to process. default: "/"
+
+- ### `baseFilePath`  - string
+
+  optional file path that corresponds with baseUrlPath
+
+- ### `filter`  - string
+
+  optional minimatch filter run against url path (not including baseUrlPath)
--- cssc.js
+++ cssc.js
@@ -1,38 +1,51 @@
-module.exports = function(opts){
-function(req, res, next) {
+var promise = require('q')
+var path = require('path')
+var parseUrl = require('parseurl')
+var fs = require('fs')
+var readdir = promise.denodeify(fs.readdir)
+var filter = require('minimatch')
 
-            if (req.method !== 'GET' && req.method !== 'HEAD') 
-                return next();
-            // Drop trailing / that wasn't in the original url
-            //if (urlPath.substr(-1) !== parseUrl.original(req).pathname.substr(-1)) 
-            //  urlPath = urlPath.slice(0,-1);
-            // change '.'
+module.exports = function (opts) {
+  opts = opts || { filter: '**/*.js', baseUrlPath: 'app', baseFilePath: 'app'};
+  var baseUrlPath = opts.baseUrlPath || '/'; // for use if there is a relative path not reflected by the fs
+  if(!baseUrlPath.startsWith('/')) baseUrlPath = '/' + baseUrlPath;
+  var baseFilePath = opts.baseFilePath || process.cwd(); // this will be used as the path to locate files from (request.path - relativeUrl)
+  if (!path.isAbsolute(baseFilePath))
+    baseFilePath = path.join(process.cwd(), baseFilePath);
 
-            var filePath = path.join(process.cwd(), parseUrl(req).pathname);
-            var pathInfo = path.parse(filePath);
-            var pathAcc = '';
-            var parts = [pathInfo.root];
-            parts.push.apply(parts, pathInfo.dir.slice(pathInfo.root.length).split(path.sep));
-            parts.push(pathInfo.base);
-            for(var i=0;i<parts.length - 1;i++){
-                if(parts[i] == '') continue;
-                pathAcc += parts[i];
-                if(i>0) pathAcc+=path.sep;
+  return function (req, res, next) {
+    if (req.method !== 'GET' && req.method !== 'HEAD')
+      return next();
 
-                (function(i,parts,pathAcc,filePath){
-                    fs.readdir(pathAcc, function(err, files){
-                        if(err){
-                            console.warn('Error checking case sensitive name at"' + pathAcc + '" for "' + filePath + '"');
-                            console.warn(err);
-                            return;
-                        }
-                        var lcFiles = files.map(function(file){ return file.toLowerCase();});
-                        var lcIndex = lcFiles.indexOf(parts[i + 1].toLowerCase());
-                        if(files.indexOf(parts[i + 1]) === -1 && lcIndex !== -1)
-                            console.error('Case Mismatch "' + parts[i + 1] + '" does not match "' + files[lcIndex] + '" for file "' + filePath + '"')
-                    });
-                })(i,parts,pathAcc,filePath)
-            }
-            next();
-        }
-        }
+    var requestPath = parseUrl(req).pathname;
+    if (!!baseUrlPath && !requestPath.startsWith(baseUrlPath))
+      return next();
+
+    requestPath = requestPath.slice((baseUrlPath || '').length);
+    if(opts.filter && !filter(requestPath, opts.filter))
+      return next();
+
+    var currentFilePath = baseFilePath;
+    var parts = requestPath.split('/');
+    for (var i = 0; i < parts.length; i++) {
+      if (parts[i] == '') continue;
+      (function (parts, i) {
+        readdir(currentFilePath)
+          .then(function (files) {
+            var lcFiles = files.map(function (file) {
+              return file.toLowerCase();
+            });
+            var lcIndex = lcFiles.indexOf(parts[i].toLowerCase());
+            if (files.indexOf(parts[i]) === -1 && lcIndex !== -1)
+              console.error('Case Mismatch "' + parts[i] + '" does not match "' + files[lcIndex] + '" for request "' + requestPath + '"')
+          })
+          .catch(function (err) {
+            console.warn('Error checking case sensitive name at"' + currentFilePath + '" for "' + requestPath + '"');
+            console.warn(err);
+          });
+      })(parts, i);
+      currentFilePath += path.sep + parts[i];
+    }
+    next();
+  }
+}
